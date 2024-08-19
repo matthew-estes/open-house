43ae7fb6cb08b9f39f70b6eb1f53c870142606e7
matthew-estes <98664968+matthew-estes@users.noreply.github.com>
2024-08-19 15:05:35 -0400

    Add index functionality for listings, apply populate() method to include owner data, and render populated listings on the index page

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,27 +1,34 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Listing = require('../models/listing');
+const Listing = require("../models/listing");
 
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const listings = await Listing.find();  
-    res.render('listings/index.ejs', { listings });  
+    const populatedListings = await Listing.find({}).populate('owner');
+    console.log('Populated Listings:', populatedListings);
+    res.render("listings/index.ejs", {listings: populatedListings})
   } catch (err) {
-    console.log(err);  
-    res.status(500).send('Error retrieving listings');
+    console.log(err);
+    res.redirect('/');
   }
 });
 
+router.get("/new", async (req, res) => {
+  try {
+    res.render("listings/new.ejs");
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Error retrieving form");
+  }
+});
 
-router.get('/new', async (req, res) => {
-    try {
-    res.render('listings/new.ejs');   
-    } catch (error) {
-        console.log(err);  
-        res.status(500).send('Error retrieving form');   
-    }
-})
+router.post("/", async (req, res) => {
+  req.body.owner = req.session.user._id;
+  await Listing.create(req.body);
+  res.redirect("/listings");
+});
+
+
+
 
 module.exports = router;
